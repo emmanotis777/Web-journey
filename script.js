ba5aa0eae4f9f0707a10bef4d6c49a5b5b729df0
Emman Otis <1stn.otis@gmail.com>
2026-07-26 14:17:09 +0100

    Day 9: dynamic project card generated from JS data array

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const projects =[
+    {
+        title: "Personal Bio Site",
+        description: "My first website - built from scratch to learn HTML, CSS, and flexbox layout.",
+    link:
+"https://github.com/emmanotis777/web-journey"
+},
+{
+    title: "To-Do List",
+    description: "My first website - rebuild a site faster with less hand holding.",
+    link:
+    "https://github.com/emmanotis777/web-journey"
+}
+]; 
+const projectsContainer = document.getElementById("projects-container");
+projects.forEach(function(project) 
+{
+    const projectElement = document.createElement("div");
+    projectElement.className = "project-card";
+    projectElement.innerHTML = `
+    <h3>${project.title}</h3>
+    <p>${project.description}</p>
+    <a href="${project.link}" target="_blank">View Project on Github</a>`;
+    projectsContainer.appendChild(projectElement)
+});
 const themeToggle = document.getElementById("theme-toggle");
 themeToggle.addEventListener("click",function() {
     document.body.classList.toggle("light-mode");
